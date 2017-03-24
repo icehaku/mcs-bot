1a877ec8fb558819341b65eb03984db8ad868b9e
Elano Garcez <elanogarcez@gmail.com>
2017-03-24 09:36:08 -0300

    add image score to inline list

--- app/controllers/concerns/inline_bot.rb
+++ app/controllers/concerns/inline_bot.rb
@@ -1,20 +1,6 @@
 module InlineBot
   extend ActiveSupport::Concern
 
-  def get_metacritic_image_userscore_by_url(url)
-  	begin
-  		user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2"
-  		url = "http://www.metacritic.com#{url}"
-  		page = Nokogiri::HTML(open(url, 'User-Agent' => user_agent), nil, "UTF-8")
-  		image = page.css("img.product_image.large_image")[0].values[1]
-  		userscore = page.css("div.product_scores").css("div.metascore_w.user.large.game")[0].text
-  		[image, userscore]
-  	rescue
-  		["", ""]
-  	end
-  end
-
-
 	def inline(params)
 		query = ""
  		inline_query_id = ""
@@ -47,6 +33,12 @@ module InlineBot
 					game["description"] = result.css("p.deck.basic_stat").text
 					game["url"] = result.css("a")[0]['href']
 					game["url"] = "http://www.metacritic.com#{game["url"]}"
+          if game["metascore"].present?
+            game["image"] = set_metascore_image(game["metascore"])
+            game["metascore"] = "MS:#{game['metascore']}"            
+          else
+            game["image"] = "http://www.headslinger.com/feed_img/1000565.jpg"
+          end          
 					#game["image_userscore"] = get_metacritic_image_userscore_by_url(game["url"])
 
 					games << game
@@ -60,10 +52,10 @@ module InlineBot
     results = games.map.with_index do |game, index|
       {
         type: "article",
-        title: "#{game['name']}(#{game['platform']}) MS:#{game['metascore']}",
+        title: "#{game['name']}(#{game['platform']}) #{game['metascore']}",
         id: index.to_s,
-        thumb_url: "http://www.headslinger.com/feed_img/1000565.jpg",
         description: game["description"],
+        thumb_url: game["image"],
         input_message_content: {
           parse_mode: "HTML",
           message_text: "\xF0\x9F\x91\xBE <b>#{game['name']}</b>\n<b>Console</b>: #{game['platform']}\n<b>Metascore</b>: #{game['metascore']}\n<b>Release</b>: #{game['release']}\n#{game['description']}\n#{game['url']}",
@@ -74,7 +66,30 @@ module InlineBot
     bot.answer_inline_query inline_query_id: inline_query_id ,results: results
 	end
 
+
+  def get_metacritic_image_userscore_by_url(url)
+    begin
+      user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2"
+      url = "http://www.metacritic.com#{url}"
+      page = Nokogiri::HTML(open(url, 'User-Agent' => user_agent), nil, "UTF-8")
+      image = page.css("img.product_image.large_image")[0].values[1]
+      userscore = page.css("div.product_scores").css("div.metascore_w.user.large.game")[0].text
+      [image, userscore]
+    rescue
+      ["", ""]
+    end
+  end
+
+
+  def set_metascore_image(score)
+    grade = score.to_i
+
+    if grade > 75
+      "https://placeholdit.imgix.net/~text?txtsize=33&txt=#{score}&150&w=75&h=75&bg=33cc33&txtclr=ffffff"
+    elsif grade > 45
+      "https://placeholdit.imgix.net/~text?txtsize=33&txt=#{score}&150&w=75&h=75&bg=fed731&txtclr=ffffff"
+    else
+      "https://placeholdit.imgix.net/~text?txtsize=33&txt=#{score}&150&w=75&h=75&bg=cc0000&txtclr=ffffff"
+    end
+  end
 end
-
-
-
