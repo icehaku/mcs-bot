bf7a27250aaad01ec6985ebdb7b58897ba619532
Elano Garcez <elanogarcez@gmail.com>
2017-08-10 11:32:53 -0300

    improving code

--- app/controllers/concerns/inline_bot.rb
+++ app/controllers/concerns/inline_bot.rb
@@ -2,69 +2,61 @@ module InlineBot
   extend ActiveSupport::Concern
 
 	def inline(params)
-		query = ""
- 		inline_query_id = ""
+    token = '350328660:AAGWpPvLjrHihatk_OhCioDWGHEyjYh2Pts' #Metacritic Game Score Bot
+    bot = Telegram::Bot::Client.new(token)
 
     if params["inline_query"].present?
     	inline_query_id = params["inline_query"]["id"]
     	query = params["inline_query"]["query"]
-    end		
-	
+    end
+
 		url = URI.encode("http://www.metacritic.com/search/all/#{query}/results")
 		user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2"
 
 		begin
-			results = Nokogiri::HTML(open(url, 'User-Agent' => user_agent), nil, "UTF-8")
-    rescue OpenURI::HTTPError => ex
-    	results = nil
+			html_result = Nokogiri::HTML(open(url, 'User-Agent' => user_agent), nil, "UTF-8")
+    rescue
+    	html_result = nil
     end
 
-    games = []
-    if results.present?
-    	results = results.css("li.result")
+    if html_result.present?
+    	html_result = html_result.css("li.result")
 
-	    results.each do |result|
-	    	if result.css("div.result_type").css("strong").text == "Game"
-					game = Hash.new()
-					game["name"] = result.css("a").text
-					game["release"] = result.css("li.stat.release_date").css("span.data").text
-					game["platform"] = result.css("span.platform").text
-					game["metascore"] = result.css("span.metascore_w").text
-					game["description"] = result.css("p.deck.basic_stat").text
-					game["url"] = result.css("a")[0]['href']
-					game["url"] = "http://www.metacritic.com#{game["url"]}"
-          if game["metascore"].present?
-            game["image"] = set_metascore_image(game["metascore"])
-            game["metascore"] = "#{game['metascore']}"            
-          else
-            game["image"] = "http://www.headslinger.com/feed_img/1000565.jpg"
-          end          
-					#game["image_userscore"] = get_metacritic_image_userscore_by_url(game["url"])
+      games = parse_scraped_games(html_result)
+      telegram_inline_result = create_telegram_inline_result(games)
 
-					games << game
-				end
-	    end
+      bot.answer_inline_query inline_query_id: inline_query_id, results: telegram_inline_result
 	  end
+	end
 
-    token = '350328660:AAGWpPvLjrHihatk_OhCioDWGHEyjYh2Pts' #Ice Debug Bot
-    bot = Telegram::Bot::Client.new(token)
 
-    results = games.map.with_index do |game, index|
-      {
-        type: "article",
-        title: "#{game['name']}(#{game['platform']}) MS:#{game['metascore']}",
-        id: index.to_s,
-        description: game["description"],
-        thumb_url: game["image"],
-        input_message_content: {
-          parse_mode: "HTML",
-          message_text: "\xF0\x9F\x91\xBE <b>#{game['name']}</b>\n<b>Console</b>: #{game['platform']}\n<b>Metascore</b>: #{game['metascore']}\n<b>Release</b>: #{game['release']}\n#{game['description']}\n#{game['url']}",
-        },
-      }
+  def parse_scraped_games(results)
+    games = []
+
+    results.each do |result|
+      if result.css("div.result_type").css("strong").text == "Game"
+        game = Hash.new()
+        game["name"] = result.css("a").text
+        game["release"] = result.css("li.stat.release_date").css("span.data").text
+        game["platform"] = result.css("span.platform").text
+        game["metascore"] = result.css("span.metascore_w").text
+        game["description"] = result.css("p.deck.basic_stat").text
+        game["url"] = result.css("a")[0]['href']
+        game["url"] = "http://www.metacritic.com#{game["url"]}"
+        if game["metascore"].present?
+          game["image"] = set_metascore_image(game["metascore"])
+          game["metascore"] = "#{game['metascore']}"
+        else
+          game["image"] = "http://www.headslinger.com/feed_img/1000565.jpg"
+        end
+        #game["image_userscore"] = get_metacritic_image_userscore_by_url(game["url"])
+
+        games << game
+      end
     end
 
-    bot.answer_inline_query inline_query_id: inline_query_id ,results: results
-	end
+    return games
+  end
 
 
   def get_metacritic_image_userscore_by_url(url)
@@ -72,7 +64,7 @@ module InlineBot
       user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2"
       url = "http://www.metacritic.com#{url}"
       page = Nokogiri::HTML(open(url, 'User-Agent' => user_agent), nil, "UTF-8")
-      image = page.css("img.product_image.large_image")[0].values[1]    
+      image = page.css("img.product_image.large_image")[0].values[1]
       userscore = page.css("div.product_scores").css("div.metascore_w.user.large.game")[0].text
       [image, userscore]
     rescue
@@ -92,4 +84,22 @@ module InlineBot
       "https://placeholdit.imgix.net/~text?txtsize=33&txt=#{score}&150&w=75&h=75&bg=cc0000&txtclr=ffffff"
     end
   end
+
+
+  def create_telegram_inline_result(games)
+    games.map.with_index do |game, index|
+      {
+        type: "article",
+        title: "#{game['name']}(#{game['platform']}) MS:#{game['metascore']}",
+        id: index.to_s,
+        description: game["description"],
+        thumb_url: game["image"],
+        input_message_content: {
+          parse_mode: "HTML",
+          message_text: "\xF0\x9F\x91\xBE <b>#{game['name']}</b>\n<b>Console</b>: #{game['platform']}\n<b>Metascore</b>: #{game['metascore']}\n<b>Release</b>: #{game['release']}\n#{game['description']}\n#{game['url']}",
+        },
+      }
+    end
+  end
+
 end
--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -3,14 +3,11 @@ require 'net/http'
 require 'uri'
 
 class TelegramController < ApplicationController
-	include Metacritic
-	include Bot
 	include InlineBot
 
   skip_before_action :verify_authenticity_token
 
   def callback
-    #observer(params) 
     inline(params)
 
     render body: nil, head: :ok
